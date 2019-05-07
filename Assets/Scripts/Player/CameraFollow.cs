844cd99348d23ae4c8b438aec2b11114ecc8e7aa
lyj <hnulyj@qq.com>
2019-05-07 22:49:53 +0800

    Init

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 控制摄像机注视人物
+ * */
+
+public class CameraFollow : MonoBehaviour
+{
+    public float mouseSpeedX = 10;
+    public float mouseSpeedY = -10;
+    public float maxAngleY = 50;
+    public float minAngleY = -20;
+    public float distance = 5;
+    
+    float x = 0;
+    float y = 0;
+
+    void LateUpdate()
+    {
+        Camera.main.transform.LookAt(this.transform);
+
+        x += Input.GetAxis("Mouse X") * mouseSpeedX * Time.deltaTime;
+        y += Input.GetAxis("Mouse Y") * mouseSpeedY * Time.deltaTime;
+        if (y >= maxAngleY)
+            y = maxAngleY;
+        else if (y <= minAngleY)
+            y = minAngleY;
+        Quaternion q = Quaternion.Euler(y, x, 0);
+        Vector3 direction = q * Vector3.forward;
+
+        this.transform.position = this.transform.parent.position - direction * distance;
+        this.transform.LookAt(this.transform.parent);
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 控制人物移动
+ * */
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController player;
+    Animator animator;
+    Vector3 moveDirection;
+
+    private float walkSpeed = 3;
+    private float runSpeed = 10;
+    private float backSpeed = -2;
+    private float gravity = 9.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+        animator = this.transform.GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //在空中的时候不能控制
+        if (player.isGrounded)
+        {
+            //WASD控制
+            if (Input.GetKey(KeyCode.W))
+            {
+                Quaternion q = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
+                this.transform.rotation = q;
+                moveDirection = new Vector3(0.0f, 0.0f, 1);
+                moveDirection = transform.TransformDirection(moveDirection);
+
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    moveDirection = moveDirection * runSpeed;
+                    animator.SetFloat("speed", 5);
+                }
+                else
+                {
+                    moveDirection = moveDirection * walkSpeed;
+                    animator.SetFloat("speed", 3);
+                }
+
+
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                Quaternion q = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y + 180, 0);
+                this.transform.rotation = q;
+                moveDirection = new Vector3(0.0f, 0.0f, -1);
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection = moveDirection * backSpeed;
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    moveDirection = moveDirection * runSpeed;
+                    animator.SetFloat("speed", 5);
+                }
+                else
+                {
+                    moveDirection = moveDirection * walkSpeed;
+                    animator.SetFloat("speed", 3);
+                }
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+
+                Quaternion q = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y - 90, 0);
+                this.transform.rotation = q;
+                moveDirection = new Vector3(0.0f, 0.0f, 1);
+                moveDirection = transform.TransformDirection(moveDirection);
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    moveDirection = moveDirection * runSpeed;
+                    animator.SetFloat("speed", 5);
+                }
+                else
+                {
+                    moveDirection = moveDirection * walkSpeed;
+                    animator.SetFloat("speed", 3);
+                }
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                Quaternion q = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y + 90, 0);
+                this.transform.rotation = q;
+                moveDirection = new Vector3(0.0f, 0.0f, 1);
+                moveDirection = transform.TransformDirection(moveDirection);
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    moveDirection = moveDirection * runSpeed;
+                    animator.SetFloat("speed", 5);
+                }
+                else
+                {
+                    moveDirection = moveDirection * walkSpeed;
+                    animator.SetFloat("speed", 3);
+                }
+            }
+            else
+            {
+                moveDirection = Vector3.zero;
+                animator.SetFloat("speed", 0);
+            }
+
+        }
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        player.Move(moveDirection * Time.deltaTime);
+    }
+}
