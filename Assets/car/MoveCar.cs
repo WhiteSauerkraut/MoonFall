1ea57dcaea181975c544a43e10a28fe51a5aa9cf
lyj <hnulyj@qq.com>
2019-05-10 00:34:39 +0800

    加入车辆控制(未实现刹车)

---
+++ Assets/car/MoveCar.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MoveCar : MonoBehaviour
+{
+    // 获取四个车轮
+    public GameObject BRWheel;     	
+    public GameObject BLWheel;
+    public GameObject FRWheel;
+    public GameObject FLWheel;
+
+    // 车轮渲染对象
+    public GameObject BRwheelMesh;
+    public GameObject BLwheelMesh;
+    public GameObject FRwheelMesh;
+    public GameObject FLwheelMesh;
+
+    // 车轮碰撞器
+    private WheelCollider BRwheelCollider;
+    private WheelCollider BLwheelCollider;
+    private WheelCollider FRwheelCollider;
+    private WheelCollider FLwheelCollider;
+
+    // 用于设置刹车力矩的大小
+    public float stop_speed = 100;
+
+    // 获取刚体对象
+    private Rigidbody car_rigidbody;
+
+    void Start()
+    {
+        BRwheelCollider = BRWheel.GetComponent<WheelCollider>();
+        BLwheelCollider = BLWheel.GetComponent<WheelCollider>();
+        FRwheelCollider = FRWheel.GetComponent<WheelCollider>();
+        FLwheelCollider = FLWheel.GetComponent<WheelCollider>();
+
+        // 设置重心
+        car_rigidbody = GetComponent<Rigidbody>();
+        car_rigidbody.centerOfMass = Vector3.zero;
+    }
+
+    void Uptate()
+    {
+        UpdateMeshPositions();
+
+        // 限制最大车速
+        car_rigidbody.drag = car_rigidbody.velocity.magnitude / 250;
+
+        // 空格刹车
+        if (Input.GetKey(KeyCode.Space))
+        {
+            car_rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+            car_rigidbody.constraints = RigidbodyConstraints.FreezePositionX;
+            car_rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // 前轮转向
+        float steer = Input.GetAxis("Horizontal");
+        FRwheelCollider.steerAngle = steer * 30;
+        FLwheelCollider.steerAngle = steer * 30;
+
+        // 后轮驱动
+        float accelerate = Input.GetAxis("Vertical"); ;
+        BRwheelCollider.motorTorque = accelerate * 1000;
+        BLwheelCollider.motorTorque = accelerate * 1000;
+    }
+
+    void UpdateMeshPositions()
+    {
+        Vector3 pos;
+        Quaternion quat;
+
+        BRwheelCollider.GetWorldPose(out pos, out quat);
+        BRwheelMesh.transform.position = pos;
+        BRwheelMesh.transform.rotation = quat;
+
+        BLwheelCollider.GetWorldPose(out pos, out quat);
+        BLwheelMesh.transform.position = pos;
+        BLwheelMesh.transform.rotation = quat;
+
+        FRwheelCollider.GetWorldPose(out pos, out quat);
+        FRwheelMesh.transform.position = pos;
+        FRwheelMesh.transform.rotation = quat;
+
+        FLwheelCollider.GetWorldPose(out pos, out quat);
+        FLwheelMesh.transform.position = pos;
+        FLwheelMesh.transform.rotation = quat;
+
+    }
+}
---
+++ Assets/car/WheelRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class WheelRotate : MonoBehaviour
+{
+    public GameObject wheel;            //声明游戏对象变量，用于获取挂有车轮碰撞器的对象
+    private float wheelAngle;           	//声明float类型的变量，用于设置车轮旋转角
+    private WheelCollider wheelCollider;   							//声明车轮碰撞器变量
+    void Awake()
+    {
+        wheelCollider = wheel.transform.GetComponent<WheelCollider>();  //获取车轮碰撞器
+    }
+    void Update()
+    {
+        this.transform.rotation = wheelCollider.transform.rotation * Quaternion.Euler(wheelAngle, 0, 0);    //修改当前车轮对象的旋转角度，仅绕x轴旋转
+        wheelAngle += wheelCollider.rpm * 360 / 60 * Time.deltaTime;    //计算车轮每秒旋转多少度
+    }
+}
+
