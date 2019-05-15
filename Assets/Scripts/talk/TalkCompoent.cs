bdc5598025b641f95fc963dba9739a141508a8a7
lyj <hnulyj@qq.com>
2019-05-16 00:40:31 +0800

    场景增加NPC

---
+++ Assets/Scripts/talk/TalkCompoent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 谈话组件
+ * */
+
+public class TalkCompoent : MonoBehaviour
+{
+    // 谈话内容
+    public string[] talk_strs;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        talk_strs = new string[] { "去吧，少年！", "带着我们的共同梦想，前进吧！" };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/talk/TalkWindow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * 谈话窗口控制
+ * */
+
+public class TalkWindow : MonoBehaviour
+{
+    GameObject talk_window;
+    Text text_talk;
+    Text text_talker;
+    Text text_speaker;
+    Button btn;
+    Text hint_text;
+
+    void Init()
+    {
+        talk_window = transform.Find("talk_window").gameObject;
+        text_talk = talk_window.transform.Find("Text_talk").gameObject.GetComponent<Text>();
+        text_talker = talk_window.transform.Find("Text_talker").gameObject.GetComponent<Text>();
+        text_speaker = talk_window.transform.Find("Text_speaker").gameObject.GetComponent<Text>();
+        btn = talk_window.transform.Find("Button").gameObject.GetComponent<Button>();
+        hint_text = btn.transform.GetComponentInChildren<Text>();
+    }
+
+    void Show()
+    {
+
+    }
+
+    void Hide()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
